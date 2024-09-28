1aebba7eab264d2d557e4031b3ecb685ca989ac2
Saad Masood <saad.masood8.sm@gmail.com>
2024-09-29 04:02:48 +0500

    save

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Dashboard.css";
-import {useRef } from "react";
+import { useRef } from "react";
 
 export default function Dashboard() {
   const scrollLeft = () => {
@@ -24,7 +24,6 @@ export default function Dashboard() {
   //   scrollRightBtn.style.display = "block";
   // });
 
-
   // const day3 = document.getElementById('day3btn')
   // const day10 = document.getElementById('day10btn')
   // function changetoDay3(){
@@ -40,32 +39,28 @@ export default function Dashboard() {
   //   if(day3 && day10){
   //     day3.style.backgroundColor='transparent'
   //     day10.style.backgroundColor='#be83de'
-  //   } 
+  //   }
   // }
-
 
   const day3Ref = useRef(null);
   const day10Ref = useRef(null);
   const forecastDiv = useRef(null);
-  
+
   const changetoDay3 = () => {
     if (day3Ref.current && day10Ref.current && forecastDiv.current) {
-      day10Ref.current.style.backgroundColor = 'transparent';
-      day3Ref.current.style.backgroundColor = '#be83de';
-      forecastDiv.current.style.overflow='hidden'
-
+      day10Ref.current.style.backgroundColor = "transparent";
+      day3Ref.current.style.backgroundColor = "#be83de";
+      forecastDiv.current.style.overflow = "hidden";
     }
   };
-  
+
   const changetoDay10 = () => {
     if (day3Ref.current && day10Ref.current && forecastDiv.current) {
-      day3Ref.current.style.backgroundColor = 'transparent';
-      day10Ref.current.style.backgroundColor = '#be83de';
-      forecastDiv.current.style.overflowY='scroll'
+      day3Ref.current.style.backgroundColor = "transparent";
+      day10Ref.current.style.backgroundColor = "#be83de";
+      forecastDiv.current.style.overflowY = "scroll";
     }
   };
-
-
 
   return (
     <div>
@@ -326,23 +321,187 @@ export default function Dashboard() {
 
               <div id="forecastday">
                 <div class="toggle-switch">
-                  <button id="day3btn" ref={day3Ref} class="switch-button" onClick={changetoDay3}>3 days</button>
-                  <button id="day10btn" ref={day10Ref} class="switch-button" onClick={changetoDay10}>10 days</button>
+                  <button
+                    id="day3btn"
+                    ref={day3Ref}
+                    class="switch-button"
+                    onClick={changetoDay3}
+                  >
+                    3 days
+                  </button>
+                  <button
+                    id="day10btn"
+                    ref={day10Ref}
+                    class="switch-button"
+                    onClick={changetoDay10}
+                  >
+                    10 days
+                  </button>
                 </div>
               </div>
             </div>
 
             <div className="forecast-innerDiv" id="forecasts" ref={forecastDiv}>
-              <div className="forcastdaysitem" id="forcastdayitem1"></div>
-              <div className="forcastdaysitem" id="forcastdayitem2"></div>
-              <div className="forcastdaysitem" id="forcastdayitem3"></div>
-              <div className="forcastdaysitem" id="forcastdayitem4"></div>
-              <div className="forcastdaysitem" id="forcastdayitem5"></div>
-              <div className="forcastdaysitem" id="forcastdayitem6"></div>
-              <div className="forcastdaysitem" id="forcastdayitem7"></div>  
-              <div className="forcastdaysitem" id="forcastdayitem8"></div>
-              <div className="forcastdaysitem" id="forcastdayitem9"></div>
-              <div className="forcastdaysitem" id="forcastdayitem10"></div>
+              <div className="forecastdaysitem" id="forcastdayitem1">
+                <div className="forecastitemtextDiv">
+                  <span className="material-symbols-outlined">cloud</span>
+                  <div>
+                    <span className="humidityValue forecasttextSpan">+24*</span>
+                    <span className="humidityValue forecasttextSpan">/14*</span>
+                  </div>
+                </div>
+                <div className="forecastitemdateDiv">
+                  <div>
+                    <span className="forcastdatespan">16</span>
+                    <span className="forcastdatespan">May,Tue</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="forecastdaysitem" id="forcastdayitem2">
+                <div className="forecastitemtextDiv">
+                  <span className="material-symbols-outlined">sunny</span>
+                  <div>
+                    <span className="humidityValue forecasttextSpan">+24*</span>
+                    <span className="humidityValue forecasttextSpan">/14*</span>
+                  </div>
+                </div>
+                <div className="forecastitemdateDiv">
+                  <div>
+                    <span className="forcastdatespan">17</span>
+                    <span className="forcastdatespan">May,Wed</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="forecastdaysitem" id="forcastdayitem3">
+                <div className="forecastitemtextDiv">
+                  <span className="material-symbols-outlined">thunderstorm</span>
+                  <div>
+                    <span className="humidityValue forecasttextSpan">+24*</span>
+                    <span className="humidityValue forecasttextSpan">/14*</span>
+                  </div>
+                </div>
+                <div className="forecastitemdateDiv">
+                  <div>
+                    <span className="forcastdatespan">18</span>
+                    <span className="forcastdatespan">May,Thu</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="forecastdaysitem" id="forcastdayitem4">
+                {" "}
+                <div className="forecastitemtextDiv">
+                  <span className="material-symbols-outlined">partly_cloudy_night</span>
+                  <div>
+                    <span className="humidityValue forecasttextSpan">+24*</span>
+                    <span className="humidityValue forecasttextSpan">/14*</span>
+                  </div>
+                </div>
+                <div className="forecastitemdateDiv">
+                  <div>
+                    <span className="forcastdatespan">19</span>
+                    <span className="forcastdatespan">May,Fri</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="forecastdaysitem" id="forcastdayitem5">
+                <div className="forecastitemtextDiv">
+                  <span className="material-symbols-outlined">foggy</span>
+                  <div>
+                    <span className="humidityValue forecasttextSpan">+24*</span>
+                    <span className="humidityValue forecasttextSpan">/14*</span>
+                  </div>
+                </div>
+                <div className="forecastitemdateDiv">
+                  <div>
+                    <span className="forcastdatespan">20</span>
+                    <span className="forcastdatespan">May,Sat</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="forecastdaysitem" id="forcastdayitem6">
+                <div className="forecastitemtextDiv">
+                  <span className="material-symbols-outlined">sunny</span>
+                  <div>
+                    <span className="humidityValue forecasttextSpan">+24*</span>
+                    <span className="humidityValue forecasttextSpan">/14*</span>
+                  </div>
+                </div>
+                <div className="forecastitemdateDiv">
+                  <div>
+                    <span className="forcastdatespan">21</span>
+                    <span className="forcastdatespan">May,Sun</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="forecastdaysitem" id="forcastdayitem7">
+                <div className="forecastitemtextDiv">
+                  <span className="material-symbols-outlined">sunny</span>
+                  <div>
+                    <span className="humidityValue forecasttextSpan">+24*</span>
+                    <span className="humidityValue forecasttextSpan">/14*</span>
+                  </div>
+                </div>
+                <div className="forecastitemdateDiv">
+                  <div>
+                    <span className="forcastdatespan">22</span>
+                    <span className="forcastdatespan">May,Mon</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="forecastdaysitem" id="forcastdayitem8">
+                <div className="forecastitemtextDiv">
+                  <span className="material-symbols-outlined">cloud</span>
+                  <div>
+                    <span className="humidityValue forecasttextSpan">+24*</span>
+                    <span className="humidityValue forecasttextSpan">/14*</span>
+                  </div>
+                </div>
+                <div className="forecastitemdateDiv">
+                  <div>
+                    <span className="forcastdatespan">23</span>
+                    <span className="forcastdatespan">May,Tue</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="forecastdaysitem" id="forcastdayitem9">
+                <div className="forecastitemtextDiv">
+                  <span className="material-symbols-outlined">rainy</span>
+                  <div>
+                    <span className="humidityValue forecasttextSpan">+24*</span>
+                    <span className="humidityValue forecasttextSpan">/14*</span>
+                  </div>
+                </div>
+                <div className="forecastitemdateDiv">
+                  <div>
+                    <span className="forcastdatespan">24</span>
+                    <span className="forcastdatespan">May,Wed</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="forecastdaysitem" id="forcastdayitem10">
+                <div className="forecastitemtextDiv">
+                  <span className="material-symbols-outlined">thunderstorm</span>
+                  <div>
+                    <span className="humidityValue forecasttextSpan">+24*</span>
+                    <span className="humidityValue forecasttextSpan">/14*</span>
+                  </div>
+                </div>
+                <div className="forecastitemdateDiv">
+                  <div>
+                    <span className="forcastdatespan">25</span>
+                    <span className="forcastdatespan">May,Thu</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
